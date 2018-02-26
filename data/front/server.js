578d96cb67063f3f02079734d52e936ada1d28e3
Christos @actondev <chris.actondev@gmail.com>
2018-02-26 02:18:50 +0100

    WIP adding some front

---
+++ data/front/server.js
@@ -0,0 +1,20 @@
+var redis = require("redis")
+  , subscriber = redis.createClient(6379, "redis_db")
+  , publisher  = redis.createClient(6379, "redis_db")
+  , express = require("express")
+  , app = express()
+
+
+publisher.publish("front:log", "hi");
+
+app.get('/', function (req, res, next) {
+  try {
+    res.send(hi)
+  } catch (e) {
+    next(e)
+  }
+})
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Listening on http://localhost:' + (process.env.PORT || 3000))
+})
