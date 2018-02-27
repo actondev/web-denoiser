faa20be136055788e55379b24677a0128c523d47
Christos @actondev <chris.actondev@gmail.com>
2018-02-28 00:32:09 +0100

    WIP drawing the waveform

--- data/front/server.js
+++ data/front/server.js
@@ -3,19 +3,30 @@ var redis = require("redis")
   , publisher  = redis.createClient(6379, "redis_db")
   , express = require("express")
   , app = express()
+  , path = require("path")
 
 
 publisher.publish("front:log", "hi");
 
 app.get('/', function (req, res, next) {
   try {
-    res.send("hi")
+    res.sendFile(path.join(__dirname + '/web/index.html'));
   } catch (e) {
     next(e)
   }
 })
 
+app.use(function (req, res, next) {
+  var filename = path.basename(req.url);
+  var extension = path.extname(filename);
+  console.log(filename);
+  next();
+});
+
+
 app.use('/public', express.static('/shared'))
+app.use('/', express.static('/web'))
+// app.use(express.static(path.join(__dirname, 'www')));
 
 app.listen(process.env.PORT || 8080, function () {
   console.log('Listening on http://localhost:' + (process.env.PORT || 8080))
