21762aedd028982f13dcbeb086b02e0665d2e749
Christos @actondev <chris.actondev@gmail.com>
2018-02-28 20:14:24 +0100

    uniq id for component canvas

--- data/front/web/component.js
+++ data/front/web/component.js
@@ -1,13 +1,17 @@
 export default {
   template: `
     <div>
-     <h1>Single-file JavaScript Component</h1>
-     <p>{{ message }}</p>
+     <canvas :id="id" width="240" height="100"></canvas>
+     <p>{{ message }} {{id}}</p>
     </div>
   `,
   data() {
     return {
-      message: 'Oh hai from the component'
+      message: 'Oh hai from the component',
+      id: null
     }
+  },
+  mounted () {
+    this.id = this._uid
   }
 }
