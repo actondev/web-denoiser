7d437c17d776f0938bd5cec265159f4797f74e99
Christos @actondev <chris.actondev@gmail.com>
2018-03-01 00:47:03 +0100

    socket headache

--- data/front/server.js
+++ data/front/server.js
@@ -1,10 +1,27 @@
 var redis = require("redis")
   , subscriber = redis.createClient(6379, "redis_db")
-  , publisher  = redis.createClient(6379, "redis_db")
+  , publisher = redis.createClient(6379, "redis_db")
   , express = require("express")
   , app = express()
   , path = require("path")
+  , http = require('http').Server(app)
+  , io = require('socket.io')(http);
 
+console.log("hi man")
+
+io.on('connection', function(socket){
+  client = socket.client
+  clientId = socket.client.conn.id
+  publisher.hset('people', clientId, "client");  
+  socket.on('chat message', function(msg){
+    publisher.publish("font:messages", msg);
+    io.emit('chat message', msg);
+    console.log('message: ' + msg);
+    socket.send("PRVT " + socket.client.conn.id)
+  });
+
+
+});
 
 publisher.publish("front:log", "hi");
 
@@ -37,6 +54,6 @@ app.use('/public', express.static('/shared'))
 app.use('/', express.static(path.join(__dirname, 'web')))
 app.use('/vendor', express.static(path.join(__dirname, 'vendor')))
 
-app.listen(process.env.PORT || 8080, function () {
+http.listen(process.env.PORT || 8080, function () {
   console.log('Listening on http://localhost:' + (process.env.PORT || 8080))
 })
